70f5f172264ea280b619591ab22cd26f4d46159e
Shyla Nott <shyla.nott@gmail.com>
2019-12-16 21:44:50 -0800

    update about.js

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -7,6 +7,8 @@ import SEO from '../components/seo'
 import Layout from '../containers/layout'
 import {mapEdgesToNodes, filterOutDocsWithoutSlugs} from '../lib/helpers'
 import {responsiveTitle1} from '../components/typography.module.css'
+import Footer from './Footer'
+
 
 
 const AboutPage = props => {
@@ -26,12 +28,12 @@ const AboutPage = props => {
       <Container>
         <h1 className={responsiveTitle1}>Hi, I'm Shyla</h1>
         <p>I'm a digital marketer with a passion for data science and analytics with a side of web design and development. I worked in journalism as a radio and web producer for five years gaining experience in a range of skills from design and development to project and client management. I keep my skills sharp by regularly contributing to open source and side projects.
-Outside of work I enjoy playing pick-up basketball, floral design and exploring southern California with my husband.
-Want to know more?</p> 
-        <p>Send me an email and let's chat!</p>
+Outside of work I enjoy playing pick-up basketball, floral design and exploring southern California with my awesome husband.</p>
+        <p>Want to know more? Send me an email and let's chat!</p>
         <a href="mailto:shyla.nott@gmail.com"><strong>shyla.nott@gmail.com</strong></a>
         {projectNodes && projectNodes.length > 0 && <ProjectPreviewGrid nodes={projectNodes} />}
       </Container>
+      <Footer />
     </Layout>
   )
 }
