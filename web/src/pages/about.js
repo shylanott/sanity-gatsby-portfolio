33e26f81d63a42f7b0ff05dd246e1543705737a4
Shyla Nott <shyla.nott@gmail.com>
2020-01-06 15:29:49 -0800

    Update about

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -25,8 +25,9 @@ const AboutPage = props => {
       <SEO title='About' />
       <Container>
         <h1 className={responsiveTitle1}>Hi, I'm Shyla</h1>
-        <p>I'm a digital marketer with a passion for data science and analytics with a side of web design and development. I worked in journalism as a radio and web producer for five years gaining experience in a range of skills from design and development to project and client management. I keep my skills sharp by regularly contributing to open source and side projects.
-Outside of work I enjoy playing pick-up basketball, floral design and exploring southern California with my awesome husband.</p>
+        <p>I'm a web specialist with a passion for data science and analytics, web design and development, and content management. I work at inewsource, a nonproft investigative newsroom, as the web and social media manager. Before that, I was a radio producer for a live, public affairs talk show at the NPR member station in Columbus, Ohio. 
+        <p>I keep my skills sharp by regularly pursuing open source side projects <a href="https://github.com/shylanott/twitter-sentiment-analyzer">like this one</a>.</p>
+        <p>Outside of work I enjoy pick-up basketball, floral design, watching Masterpiece on PBS and exploring southern California with my awesome husband.</p>
         <p>Want to know more? Send me an email and let's chat!</p>
         <a href="mailto:shyla.nott@gmail.com"><strong>shyla.nott@gmail.com</strong></a>
         {projectNodes && projectNodes.length > 0 && <ProjectPreviewGrid nodes={projectNodes} />}
