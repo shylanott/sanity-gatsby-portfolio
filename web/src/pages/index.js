569307a19084f5a12528ca7e093b7c4206b33594
Shyla Nott <shyla.nott@gmail.com>
2020-05-18 10:19:03 -0700

    update index bio

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -93,11 +93,11 @@ const IndexPage = props => {
         <div className={styles.mainContent}>
           <h1 hidden>Welcome to {site.title}</h1>
           <h1 className={responsiveTitle1}>Hi, I'm Shyla</h1>
-          <p>I'm a web developer and designer based in San Diego. I work for inewsource as 
-            the web and social media manager. Before that, I was the assistant producer and 
-            board operator for All Sides with Ann Fisher – a live, public affairs talk show at 
-            the NPR member station in Columbus, Ohio. I earned my master's from Northwestern 
-            University's Medill School of Journalism.</p> 
+          <p>I'm a web developer and designer based in San Diego. I work for a nonprofit 
+            investigative newsroom as the web and social media manager. Before that, I was the 
+            assistant producer and board operator for All Sides with Ann Fisher – a live, public 
+            affairs talk show at the NPR member station in Columbus, Ohio. I earned my master's 
+            from Northwestern University's Medill School of Journalism.</p> 
           <p>If you'd like to get in touch, <a href="mailto:shyla.nott@gmail.com">send me an email</a> or reach out on <a href="https://www.linkedin.com/in/shylanott/">LinkedIn</a>.</p>
         </div>
         {projectNodes && (
