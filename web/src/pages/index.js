238131aa411d489b712f6031d0bf11c9569c1a94
Shyla Nott <shyla.nott@gmail.com>
2020-08-22 13:29:34 -0700

    update to about

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -96,13 +96,13 @@ const IndexPage = props => {
           <h1 hidden>Welcome to {site.title}</h1>
           <h1 className={responsiveTitle1}>Hi, I'm Shyla.</h1>
           <div>
-            <p>I'm a digital storyteller and web designer passionate about making complex information 
-              digestible, accessible and engaging. 
+            <p>I'm a digital manager, web designer and developer passionate about making complex information 
+              digestible, accessible and engaging.</p> 
               
-              I work at a nonprofit investigative newsroom in San Diego as the web and social media manager. 
-              Before that, I was the assistant producer for a live, public affairs talk show at the NPR member 
-              station in Columbus, Ohio. I earned my master's from Northwestern University's Medill School of 
-              Journalism with a focus on interactive publishing.</p>
+            <p>I work for a nonprofit investigative newsroom in San Diego as the digital content manager where I oversee the organization's core products, 
+              including the website, newsletters and social media accounts. Before that, I was the assistant producer for a live, daily public affairs talk show at WOSU Public Radio in Columbus, Ohio. 
+              I earned my master's from Northwestern University's Medill School of Journalism with a focus on interactive publishing.</p>
+
             <p>If you'd like to say hello, <a href="mailto:shyla.nott@gmail.com">send me an email</a> or reach out on <a href="https://www.linkedin.com/in/shylanott/">LinkedIn</a>.</p>
           </div>
         </div>
